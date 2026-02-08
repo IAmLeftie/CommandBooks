c576fd2984a2cc73c1aa82b139c0d52a498df06f
IAmLeftie <31018066+IAmLeftie@users.noreply.github.com>
2026-02-08 13:58:12 -0500

    0.2.0

--- CommandBooks/Items/ItemCommandBook.cs
+++ CommandBooks/Items/ItemCommandBook.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
 using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
 using Vintagestory.API.Server;
 using Vintagestory.API.Util;
+using Vintagestory.Client.NoObf;
 
 #nullable disable
 
@@ -16,14 +19,57 @@ namespace CommandBooks.Items
 {
     internal class ItemCommandBook : Item
     {
+        public SimpleParticleProperties particlesHeld;
+
+        public bool recentlyUsed;
+
         public override void OnLoaded(ICoreAPI api)
         {
             base.OnLoaded(api);
+
+            recentlyUsed = false;
+
+            particlesHeld = new SimpleParticleProperties(
+                50, 50,
+                ColorUtil.ToRgba(50, 220, 220, 220),
+                new Vec3d(),
+                new Vec3d(),
+                new Vec3f(-0.4f, 0.45f, -0.4f),
+                new Vec3f(0.4f, 1.0f, 0.4f),
+                1.5f,
+                0,
+                0.5f,
+                0.75f,
+                EnumParticleModel.Cube
+            );
         }
+
+        public void UseCheck(float dt)
+        {
+            recentlyUsed = false;
+            return;
+        }
+
+        delegate void OnInteractUsed();
         
         public override void OnHeldInteractStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handling)
         {
             if (!firstEvent) return;
+            if (byEntity.Controls.ShiftKey)
+            {
+                base.OnHeldInteractStart(slot, byEntity, blockSel, entitySel, firstEvent, ref handling);
+                return;
+            }
+            if (recentlyUsed)
+            {
+                if (api is ICoreClientAPI capi)
+                {
+                    capi.TriggerIngameError(this, "error-cooldown", "Please wait a moment before using this book again.");
+                    return;
+                }
+            }
+            recentlyUsed = true;
+            api.Event.RegisterCallback(UseCheck, 1000);
             if (slot.Itemstack.Attributes?["commands"] == null)
             {
                 slot.Itemstack.Attributes.SetString("commands", "[]");
@@ -46,12 +92,22 @@ namespace CommandBooks.Items
 
             if (byEntity.World.Side != EnumAppSide.Server)
             {
+                if (api is ICoreClientAPI capi)
+                {
+                    capi.ShowChatMessage("You activate the contents of the book...");
+
+                    if (slot.Itemstack.Attributes.GetBool("oneTimeUse") == true)
+                    {
+                        capi.ShowChatMessage("The book crumbles to dust in your hands.");
+                    }
+                }
                 handling = EnumHandHandling.PreventDefault;
                 return;
             }
 
             try
             {
+
                 List<string> commands = JsonUtil.ToObject<List<string>>(slot.Itemstack.Attributes?["commands"].ToString(), "");
 
                 bool.TryParse(slot.Itemstack.Attributes?["oneTimeUse"].ToString(), out bool oneTimeUse);
@@ -84,6 +140,10 @@ namespace CommandBooks.Items
                         slot.TakeOut(1);
                         slot.MarkDirty();
                     }
+
+                    api.World.PlaySoundAt(new AssetLocation("game", "sounds/held/bookturn3"), byEntity);
+                    SpawnParticles(api.World, byEntity.Pos.XYZ, true);
+                    
                 }
             }
             catch (Exception ex)
@@ -110,15 +170,15 @@ namespace CommandBooks.Items
                     }
                 }
 
-                if (parsedCommands.Count == 0)
+                if (inSlot.Itemstack.Attributes.GetAsBool("secret") == true)
                 {
-                    dsc.AppendLine("This book currently has no commands.");
+                    dsc.AppendLine("<i><font color=\"#919090\">The commands on this book are hidden.</font></i>");
                 }
                 else
                 {
-                    if (inSlot.Itemstack.Attributes.GetAsBool("secret") == true)
+                    if (parsedCommands.Count == 0)
                     {
-                        dsc.AppendLine("<i><font color=\"#919090\">The commands on this book are hidden.</font></i>");
+                        dsc.AppendLine("This book currently has no commands.");
                     }
                     else
                     {
@@ -130,8 +190,16 @@ namespace CommandBooks.Items
                             i++;
                         }
                         dsc.AppendLine("This book will run the following commands on use:" + formatted);
+
                     }
 
+                    if (inSlot.Itemstack.Attributes?["elevated"] != null)
+                    {
+                        if (inSlot.Itemstack.Attributes.GetAsBool("elevated") == true)
+                        {
+                            dsc.AppendLine("<strong><font color=\"#34ebe8\">Will run with elevated permissions.</font></strong>");
+                        }
+                    }
                 }
 
                 if (inSlot.Itemstack.Attributes?["oneTimeUse"] != null)
@@ -141,19 +209,29 @@ namespace CommandBooks.Items
                         dsc.AppendLine("<strong><font color=\"#eb3434\">Will be destroyed on use.</font></strong>");
                     }
                 }
-
-                if (inSlot.Itemstack.Attributes?["elevated"] != null)
-                {
-                    if (inSlot.Itemstack.Attributes.GetAsBool("elevated") == true)
-                    {
-                        dsc.AppendLine("<strong><font color=\"#34ebe8\">Will run with elevated permissions.</font></strong>");
-                    }
-                }
             }
             catch (Exception ex)
             {
                 api.Logger.Error("Failed to set commandbook item info");
                 api.Logger.Error(ex.ToString());
+            }
+        }
+
+        void SpawnParticles(IWorldAccessor world, Vec3d pos, bool final)
+        {
+            if (final || world.Rand.NextDouble() > 0.8)
+            {
+                int h = 110 + world.Rand.Next(15);
+                int v = 100 + world.Rand.Next(50);
+                particlesHeld.MinPos = pos;
+                particlesHeld.Color = ColorUtil.ReverseColorBytes(ColorUtil.HsvToRgba(h, 180, v));
+
+                particlesHeld.MinSize = 0.2f;
+                particlesHeld.ParticleModel = EnumParticleModel.Quad;
+                particlesHeld.OpacityEvolve = EvolvingNatFloat.create(EnumTransformFunction.LINEAR, -150);
+                particlesHeld.Color = ColorUtil.ReverseColorBytes(ColorUtil.HsvToRgba(h, 180, v, 150));
+
+                world.SpawnParticles(particlesHeld);
             }
         }
     }
